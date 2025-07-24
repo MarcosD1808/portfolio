83305298e23898bcd620839b84f4c444aa812c54
Marcos <marcosdiaz1808@gmail.com>
2025-07-24 18:09:42 -0300

    emailjsv2

--- main.js
+++ main.js
@@ -52,8 +52,6 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
-
-
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector(".contact-form");
 
